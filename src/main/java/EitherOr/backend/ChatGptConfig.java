ec85e8135ca6a08e57105cf4273a2bc1a2b38df6
chanhale <chan771788@gmail.com>
2023-04-12 11:31:02 +0900

    secret deleted

--- src/main/java/EitherOr/backend/ChatGptConfig.java
+++ src/main/java/EitherOr/backend/ChatGptConfig.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 public class ChatGptConfig {
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String API_KEY = "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";
+    public static final String API_KEY = "API-KEY";
     public static final String MODEL = "gpt-3.5-turbo";
     public static final Double TEMPERATURE = 0.0;
     public static final String MEDIA_TYPE = "application/json; charset=UTF-8";
